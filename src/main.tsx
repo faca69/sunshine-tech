fefd51542c9de6c68ccce7ccb125d296adb0a696
faca69 <fnikolov123456789@gmail.com>
2025-11-10 17:13:10 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <>
